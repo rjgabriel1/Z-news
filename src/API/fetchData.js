2afbe0c644b4a190aa92b2d7e5d5da797fb1751d
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-05-10 09:19:40 -0400

    update to sort article by date

--- src/API/fetchData.js
+++ src/API/fetchData.js
@@ -1,12 +1,15 @@
+require("isomorphic-fetch");
+
 const apiKey = process.env.REACT_APP_API_KEY;
 
 const config = {
   news: `https://newsapi.org/v2/everything?q=breaking%20OR%exclusive%20OR%20health&apiKey=${apiKey}`,
   crypto: `https://newsapi.org/v2/everything?q=cryptocurrency%20OR%stablecoins%20OR%trading%20OR%bitcoin%20OR%ethereum&apiKey=${apiKey}`,
   technology: `https://newsapi.org/v2/everything?q=technology%20OR%20programming%20OR%ai&apiKey=${apiKey}`,
-  travel: `https://newsapi.org/v2/everything?q=tourism%20OR%travel&apiKey=${apiKey}`,
   gaming: `https://newsapi.org/v2/everything?q=Arcade%20OR%20gaming%20OR%20esport%20OR%20multiplayers&apiKey=${apiKey}`,
 };
+
+let allArticles = [];
 
 const fetchData = async (category) => {
   const url = config[category] || config.news;
@@ -18,5 +21,34 @@ const fetchData = async (category) => {
     articles: data.articles || [],
   };
 };
+
+const updateArticles = (data) => {
+  allArticles = [...allArticles, ...data.articles];
+
+  // Sort the articles by published date in descending order
+  allArticles.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
+  // Process and store the fetched articles
+  // Add your own logic here
+  console.log(allArticles);
+};
+
+const fetchNewsData = async () => {
+  try {
+    const newsData = await fetchData("news");
+    const cryptoData = await fetchData("crypto");
+    const technologyData = await fetchData("technology");
+    const gamingData = await fetchData("gaming");
+
+    updateArticles(newsData);
+    updateArticles(cryptoData);
+    updateArticles(technologyData);
+    updateArticles(gamingData);
+  } catch (error) {
+    console.error("Error fetching news articles:", error);
+  }
+};
+
+fetchNewsData();
 
 export default fetchData;
