4ecdd3d4dc333c59f8852ece0854152f3101c9dd
rjgabriel1 <gabrieljeffersonralph@gmail.com>
2023-05-10 09:21:32 -0400

    update cover fallback path

--- src/components/News/NewsArticle.js
+++ src/components/News/NewsArticle.js
@@ -2,17 +2,27 @@ import React, { useState, useEffect } from "react";
 import fetchData from "../../API/fetchData";
 import "./NewsArticles.css";
 
-const coverPlaceholder = "https://groundwater.org/wp-content/uploads/2022/07/news-placeholder.png";
+const placeholder ="news-placeholder.png"
+  
 
 function NewsArticle({ article, darkMode }) {
-  const imageUrl = article.urlToImage || coverPlaceholder;
+  const imageUrl = article.urlToImage ? article.urlToImage : placeholder;
 
   return (
     <div className={`news ${darkMode ? "dark" : ""}`}>
-      <img className="article-image" src={imageUrl} alt="cover of the article" />
+      <img
+        className="article-image"
+        src={imageUrl}
+        alt="cover of the article"
+      />
       <h1 className="news-title">{article.title}</h1>
       <p className="news-description">{article.description}</p>
-      <a className="article-url" href={article.url} target="_blank" rel="noreferrer">
+      <a
+        className="article-url"
+        href={article.url}
+        target="_blank"
+        rel="noreferrer"
+      >
         Read article
       </a>
       <br />
